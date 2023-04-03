e108f3b4e4d130c15af90fd201bd2168747447f9
WetGrapes-mat <WetGrapes-mat>
2023-04-03 18:24:23 +0300

    add error

--- HW-1/1/main.cxx
+++ HW-1/1/main.cxx
@@ -5,9 +5,10 @@ int main(int /*argc*/, char* /*argv*/[], char** env)
 {
     using namespace std;
 
-    cout << "Hello, world!" << endl;
+    cout << "Hello, world!"
+         << endl
 
-    bool is_good = cout.good();
+        bool is_good = cout.good();
 
     int result = is_good ? EXIT_SUCCESS : EXIT_FAILURE;
     return result;
